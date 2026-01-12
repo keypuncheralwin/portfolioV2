671ecf563f5e197cac02906baa965e729151d087
Alwin George <alwingeorge11@gmail.com>
2026-01-12 15:04:16 +1000

    security: move Firebase credentials to environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics, Analytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAT_btCdp-R_RNcFItLQ7pntlAvKa7vVPM",
-  authDomain: "portfoliov2-e4f4d.firebaseapp.com",
-  projectId: "portfoliov2-e4f4d",
-  storageBucket: "portfoliov2-e4f4d.firebasestorage.app",
-  messagingSenderId: "504299172986",
-  appId: "1:504299172986:web:fbb6492dcf4c02462ff7c3",
-  measurementId: "G-BD6R5BKWY4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
